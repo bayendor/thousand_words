7225f53a5cf09944fd35fa4ec4491d537073a64e
David Bayendor <dbayendor@gmail.com>
2015-01-14 11:16:19 -0700

    Fix omniauth initializer to use .env variables

--- config/initializers/omniauth_twitter.rb
+++ config/initializers/omniauth_twitter.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.config.omniauth_provider_key,
-    Rails.application.config.omniauth_provider_secret
+  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
 
   OmniAuth.config.on_failure = Proc.new { |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
