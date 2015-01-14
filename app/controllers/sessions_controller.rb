15c288203e736be4d43bb92c54a7a71674379f70
David Bayendor <dbayendor@gmail.com>
2015-01-14 12:28:25 -0700

    Refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,23 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to '/auth/twitter'
   end
 
   def create
-    user = User.from_omniauth(env['omniauth.auth'])
+    auth = request.env['omniauth.auth']
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    reset_session
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to root_path, notice: 'Signed in!'
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+    reset_session
+    redirect_to root_path, notice: 'Signed out!'
+  end
+
+  def failure
+    redirect_to root_path, alert: 'Authentication error!'
   end
 end
