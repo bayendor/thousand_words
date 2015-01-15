f8bad63941793f749b48eacc5d98f6c8f8924d57
David Bayendor <dbayendor@gmail.com>
2015-01-14 18:38:20 -0700

    Rubocop style fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,21 +18,21 @@ class ApplicationController < ActionController::Base
   def correct_user?
     @user = User.find(params[:id])
     unless current_user == @user
-      redirect_to root_url, :alert => "Access denied."
+      redirect_to root_url, alert: 'Access denied.'
     end
   end
 
   def authenticate_user!
-    if !current_user
-      redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+    unless current_user
+      redirect_to root_url, alert: 'You need to sign in for access to this page.'
     end
   end
 end
 
-    # def current_user
-    #   begin
-    #     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    #   rescue Exception => e
-    #     nil
-    #   end
-    # end
+# def current_user
+#   begin
+#     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+#   rescue Exception => e
+#     nil
+#   end
+# end
