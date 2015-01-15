637b222029fd748399ebcddf329644c896b2bd5b
David Bayendor <dbayendor@gmail.com>
2015-01-14 18:35:08 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-
+  get 'auth/failure', to: 'sessions#failure'
+  get 'signin' => 'sessions#new', as: 'signin'
   get 'signout', to: 'sessions#destroy', as: 'signout'
   resources :tweets, only: [:new, :create]
   resources :sessions, only: [:create, :destroy]
+  resources :users, only: [:index, :show]
 
   root to: 'static_pages#home'
 end
