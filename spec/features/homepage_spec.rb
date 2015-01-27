ac71cd1da1609fc52d6fd1ba5467ad094e3fb269
David Bayendor <dbayendor@gmail.com>
2015-01-27 13:07:30 -0700

    Fix one failing spec to look for ‘Sign in’

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe 'Visiting the home page' do
-  it "has the text 'Login'" do
+  it "has the text 'Sign in'" do
     visit '/'
-    expect(page).to have_content 'Login'
+    expect(page).to have_content 'Sign in'
   end
 
   it "has the text 'Sign Up'" do
